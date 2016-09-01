33e67c1244bd7c8d0db82bcc254297412a77dfef
zhanghui <zhanghui@ablecloud.cn>
2016-09-01 14:34:31 +0800

    update ch11

--- ch11/word2/word.go
+++ ch11/word2/word.go
@@ -3,13 +3,15 @@ package word
 import "unicode"
 
 func IsPalindrome(s string) bool {
-	var letters []rune
+	//var letters []rune
+	var letters []rune = make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
 		}
 	}
-	for i := range letters {
+	n := len(letters) / 2
+	for i := 0; i < n; i++ {
 		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
