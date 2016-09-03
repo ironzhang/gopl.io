b26c3abdd55aa9e8e726b942c8712e3c98b12da7
zhanghui <zhanghui@ablecloud.cn>
2016-09-03 13:55:56 +0800

    ch12 display

---
+++ ch12/display/display_test.go
@@ -0,0 +1,200 @@
+package display
+
+import (
+	"io"
+	"net"
+	"os"
+	"reflect"
+	"sync"
+	"testing"
+)
+
+func Example_slice() {
+	Display("slice", []*int{new(int), nil})
+	// Output:
+	// Display slice ([]*int):
+	// (*slice[0]) = 0
+	// slice[1] = nil
+}
+
+func Example_nilToInterface() {
+	var w io.Writer
+	Display("w", w)
+	// Output:
+	// Display w (<nil>):
+	// w = invalid
+}
+
+func Example_ptrToInterface() {
+	var w io.Writer
+	Display("&w", &w)
+	// Output:
+	// Display &w (*io.Writer):
+	// (*&w) = nil
+}
+
+func Example_struct() {
+	Display("x", struct{ x interface{} }{3})
+	// Output:
+	// Display x (struct { x interface {} }):
+	// x.x.type = int
+	// x.x.value = 3
+}
+
+func Example_interface() {
+	var i interface{} = 3
+	Display("i", i)
+	// Output:
+	// Display i (int):
+	// i = 3
+}
+
+func Example_ptrInterface() {
+	var i interface{} = 3
+	Display("&i", &i)
+	// Output:
+	// Display &i (*interface {}):
+	// (*&i).type = int
+	// (*&i).value = 3
+}
+
+func Example_array() {
+	Display("x", [1]interface{}{3})
+	// Output:
+	// Display x ([1]interface {}):
+	// x[0].type = int
+	// x[0].value = 3
+}
+
+func Example_movie() {
+	//!+movie
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Color           bool
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	//!-movie
+	//!+strangelove
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Color:    false,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+	//!-strangelove
+	Display("strangelove", strangelove)
+
+	// We don't use an Output: comment since displaying
+	// a map is nondeterministic.
+	/*
+		//!+output
+		Display strangelove (display.Movie):
+		strangelove.Title = "Dr. Strangelove"
+		strangelove.Subtitle = "How I Learned to Stop Worrying and Love the Bomb"
+		strangelove.Year = 1964
+		strangelove.Color = false
+		strangelove.Actor["Gen. Buck Turgidson"] = "George C. Scott"
+		strangelove.Actor["Brig. Gen. Jack D. Ripper"] = "Sterling Hayden"
+		strangelove.Actor["Maj. T.J. \"King\" Kong"] = "Slim Pickens"
+		strangelove.Actor["Dr. Strangelove"] = "Peter Sellers"
+		strangelove.Actor["Grp. Capt. Lionel Mandrake"] = "Peter Sellers"
+		strangelove.Actor["Pres. Merkin Muffley"] = "Peter Sellers"
+		strangelove.Oscars[0] = "Best Actor (Nomin.)"
+		strangelove.Oscars[1] = "Best Adapted Screenplay (Nomin.)"
+		strangelove.Oscars[2] = "Best Director (Nomin.)"
+		strangelove.Oscars[3] = "Best Picture (Nomin.)"
+		strangelove.Sequel = nil
+		//!-output
+	*/
+}
+
+func Test(t *testing.T) {
+	Display("os.Stderr", os.Stderr)
+
+	var w io.Writer = os.Stderr
+	Display("&w", &w)
+
+	var locker sync.Locker = new(sync.Mutex)
+	Display("(&locker)", &locker)
+
+	Display("locker", locker)
+
+	locker = nil
+	Display("(&locker)", &locker)
+
+	if false {
+		ips, _ := net.LookupHost("golang.org")
+		Display("ips", ips)
+	}
+
+	type P *P
+	var p P
+	p = &p
+	if false {
+		Display("p", p)
+		// Output:
+		// Display p (display.P):
+		// ...stuck, no output...
+	}
+
+	// a map that contains itself
+	type M map[string]M
+	m := make(M)
+	m[""] = m
+	if false {
+		Display("m", m)
+		// Output:
+		// Display m (display.M):
+		// ...stuck, no output...
+	}
+
+	// a slice that contains itself
+	type S []S
+	s := make(S, 1)
+	s[0] = s
+	if false {
+		Display("s", s)
+		// Output:
+		// Display s (display.S):
+		// ...stuck, no output...
+	}
+
+	// a linked list that eats its own tail
+	type Cycle struct {
+		Value int
+		Tail  *Cycle
+	}
+	var c Cycle
+	c = Cycle{42, &c}
+	if false {
+		Display("c", c)
+		// Output:
+		// Display c (display.Cycle):
+		// c.Value = 42
+		// (*c.Tail).Value = 42
+		// (*(*c.Tail).Tail).Value = 42
+		// ...ad infinitum...
+	}
+
+	if false {
+		Display("rV", reflect.ValueOf(os.Stderr))
+	}
+}
